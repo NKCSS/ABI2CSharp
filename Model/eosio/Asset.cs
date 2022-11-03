824572dd1f4733ba4675414558b56da011e809f5
Nick Kusters <github@nickkusters.com>
2022-11-03 20:29:27 +0100

    Added an extra constructor and operators for Assets

--- Model/eosio/Asset.cs
+++ Model/eosio/Asset.cs
@@ -20,6 +20,11 @@ namespace Abi2CSharp.Model.eosio
 		/// </remarks>
 		public override string ToString() => $"{BalanceDecimal.ToString($"F{Token.precision}", EnglishCulture)} {Token.name}";
         public Asset() { } // Empty constructor for serializing
+		public Asset(Symbol token, UInt64 balance)
+        {
+			Token = token;
+			Balance = balance;
+        }
 		public static implicit operator Asset(string value)
 		{
 			Asset result = new Asset();
@@ -39,5 +44,22 @@ namespace Abi2CSharp.Model.eosio
 		public Asset Deserialize(JsonReader reader) => (string)reader.Value;
 		public override int GetHashCode() => ToString().GetHashCode();
 		public override bool Equals(object obj) => obj?.GetHashCode().Equals(GetHashCode()) ?? false;
+		public static Asset operator +(Asset a, Asset b)
+        {
+			System.Diagnostics.Contracts.Contract.Requires<ArgumentException>(a.Token == b.Token, "Both assets need to be the same token!");
+			return new Asset(a.Token, a.Balance + b.Balance);
+		}
+		public static Asset operator -(Asset a, Asset b)
+		{
+			System.Diagnostics.Contracts.Contract.Requires<ArgumentException>(a.Token == b.Token, "Both assets need to be the same token!");
+			return new Asset(a.Token, a.Balance - b.Balance);
+		}
+		public static Asset operator *(Asset x, ulong multi)
+			=> new Asset(x.Token, x.Balance * multi);
+		public static Asset operator /(Asset x, ulong multi)
+		{
+			System.Diagnostics.Contracts.Contract.Requires<DivideByZeroException>(multi > 0, "Cannot divide by zero");
+			return new Asset(x.Token, x.Balance / multi);
+		}
 	}
 }
